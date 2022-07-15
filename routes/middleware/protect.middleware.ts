d9e764335213b4b5a3e5b7608b919b29eeccfeb0
Marcus Aurelius <8272331@gmail.com>
2022-07-15 17:48:58 +0200

    x-auth-token => X-Auth-Token

--- routes/middleware/protect.middleware.ts
+++ routes/middleware/protect.middleware.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 import {config} from '../../config/config';
 
 const protectMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('x-auth-token');
+    const token = req.header('X-Auth-Token');
     if (!token) throw new ValidationError(ValidationError.messages.auth.notAuthorised, 401);
     try {
         await jwt.verify(token, config.jwt.privateKey);
