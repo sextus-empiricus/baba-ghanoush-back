c2f56ebd57f9a73b412cc3aa403697a5c04e9bca
Marcus Aurelius <8272331@gmail.com>
2022-07-15 15:40:21 +0200

    ready for deploy

--- config/config.ts
+++ config/config.ts
@@ -6,9 +6,9 @@ export const config = {
     db: {
         port: 3306,
         host: 'eu-cdbr-west-03.cleardb.net',
-        user: 'b8adb70e68430f',
-        password: 'd23e9ff9',
-        database: 'heroku_e0d00962635b82c'
+        user: 'bbdec548653477',
+        password: '38382a78',
+        database: 'heroku_1854870925df3da'
     },
     jwt: {
         privateKey: '2F4BCCE0464569771FAEEA317F2C9C121B1BF9D48EA3A1851A259D7A9E484C87'
